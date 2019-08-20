04e4d935d568f59fac36113b76966b1697f5048d
ZoloMohan <mohanzolomon@gmail.com>
2019-08-20 16:45:07 +0530

    Add Todo

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import React, {useState} from 'react'
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
+import React, { useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import TodoList from './TodoList';
 import Input from './Input';
 
 export default function App() {
-
 	const initialState = [
-		{id: 1, task: 'Complete Building Todo with React Hooks', completed: false},
-		{id: 2, task: 'Learn Context', completed: false},
-		{id: 3, task: 'Get Flutter Course from Avinash', completed: false}
-	]
-	const [todos, setTodos] = useState(initialState);
-
+		{ id: 1, task: 'Complete Building Todo with React Hooks', completed: false },
+		{ id: 2, task: 'Learn Context', completed: false },
+		{ id: 3, task: 'Get Flutter Course from Avinash', completed: false }
+	];
+	const [ todos, setTodos ] = useState(initialState);
+	const addTodo = (todoTask) => {
+		setTodos([ ...todos, { id: 4, task: todoTask, completed: false } ]);
+	};
 	return (
-		<Paper style={{
-			padding: 0,
-			margin: 0,
-			height: '100vh',
-			backgroundColor: '#fafafa',
-			borderRadius: 0
-		}}
-		elevation={0}
+		<Paper
+			style={{
+				padding         : 0,
+				margin          : 0,
+				height          : '100vh',
+				backgroundColor : '#fafafa',
+				borderRadius    : 0
+			}}
+			elevation={0}
 		>
-			<AppBar color='primary' position='static' style={{height: '64px'}}>
+			<AppBar color='primary' position='static' style={{ height: '64px' }}>
 				<Toolbar>
 					<Typography color='inherit'>Todos</Typography>
 				</Toolbar>
 			</AppBar>
-			<Input />
+			<Input onSubmit={addTodo} />
 			<TodoList todos={todos} />
 		</Paper>
-	)
+	);
 }
--- src/Input.js
+++ src/Input.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import TextField from '@material-ui/core/TextField'
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
 import { Paper } from '@material-ui/core';
-import useInputState from './hooks/useInputState'
+import useInputState from './hooks/useInputState';
 
-export default function Input() {
-
-	const [value, handleChange, resetValue] = useInputState('');
+export default function Input({ onSubmit }) {
+	const [ value, handleChange, resetValue ] = useInputState('');
 
 	return (
 		<Paper>
-			<TextField label='New Task' fullWidth onChange={handleChange}/>
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
+					onSubmit(value);
+					resetValue();
+				}}
+			>
+				<TextField value={value} label='New Task' fullWidth onChange={handleChange} />
+			</form>
 		</Paper>
-	)
+	);
 }
