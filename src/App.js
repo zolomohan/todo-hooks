9f7a0d3ff3a8745e49ecf56993ee3161caa13ddc
ZoloMohan <mohanzolomon@gmail.com>
2019-08-20 16:16:38 +0530

    Added Scaffold

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+export default function App() {
+	return (
+		<Paper style={{
+			padding: 0,
+			margin: 0,
+			height: '100vh',
+			backgroundColor: '#fafafa',
+			borderRadius: 0
+		}}
+		elevation={0}
+		>
+			<AppBar color='primary' position='static' style={{height: '64px'}}>
+				<Toolbar>
+					<Typography color='inherit'>Todos</Typography>
+				</Toolbar>
+			</AppBar>
+		</Paper>
+	)
 }
-
-export default App;
