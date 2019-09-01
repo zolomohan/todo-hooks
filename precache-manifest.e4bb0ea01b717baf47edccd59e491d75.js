77c3d61b73495c238158e1799965921942a23448
ZoloMohan <mohanzolomon@gmail.com>
2019-09-01 16:29:42 +0530

    Updates

---
+++ precache-manifest.e4bb0ea01b717baf47edccd59e491d75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1e1de3bc93f8977d96eb968e008026",
+    "url": "/todo-hooks/index.html"
+  },
+  {
+    "revision": "6ad6af5dce84f101bed9",
+    "url": "/todo-hooks/static/css/main.f07f3361.chunk.css"
+  },
+  {
+    "revision": "e88fb7b49993fe2622b7",
+    "url": "/todo-hooks/static/js/2.fd676649.chunk.js"
+  },
+  {
+    "revision": "6ad6af5dce84f101bed9",
+    "url": "/todo-hooks/static/js/main.893b76f6.chunk.js"
+  },
+  {
+    "revision": "13df205b46952424d377",
+    "url": "/todo-hooks/static/js/runtime~main.12f1cd5e.js"
+  }
+]);
